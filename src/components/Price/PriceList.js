f2208af7271162daf75fc36f7353c1f930dd93ee
Frederico Marques <frederico.marques.22@gmail.com>
2017-09-08 16:50:58 -0300

    consume new exchange state properly

--- src/components/Price/PriceList.js
+++ src/components/Price/PriceList.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
-import { FlatList, View } from 'react-native'
+import PropTypes from 'prop-types'
+import { FlatList, View, Text } from 'react-native'
+import { connect } from 'react-redux'
+import actions from '../../actions'
 import PriceItem from './PriceItem'
 
 class PriceList extends Component {
+  static propTypes = {
+    exchanges: PropTypes.object,
+    lastUpdate: PropTypes.number,
+    loadExchangeData: PropTypes.func
+  }
+
   static navigationOptions = {
     title: 'Welcome'
   }
+
+  componentWillMount () {
+    this.props.loadExchangeData()
+  }
+
+  renderLastUpdateTime () {
+    const {lastUpdate} = this.props
+    return <Text>{lastUpdate}</Text>
+  }
+
+  convertStateToData () {
+    const { exchanges } = this.props
+    return Object.keys(exchanges).map(k => ({ key: k, ...exchanges[k] }))
+  }
+
   renderItem = (item) => {
     return (
       <View style={{paddingBottom: 15}}>
@@ -16,11 +40,21 @@ class PriceList extends Component {
 
   render () {
     return (
-      <FlatList
-        data={[{key: 1}, {key: 2}, {key: 3}, {key: 4}, {key: 5}]}
-        renderItem={this.renderItem}
-      />
+      <View>
+        {this.renderLastUpdateTime()}
+        <FlatList
+          data={this.convertStateToData()}
+          renderItem={this.renderItem}
+        />
+      </View>
     )
   }
 }
-export default PriceList
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    exchanges: state.exchange.exchanges,
+    lastUpdate: state.exchange.lastUpdate
+  }
+}
+export default connect(mapStateToProps, actions)(PriceList)
